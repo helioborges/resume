3f52b597bf0e378a105bc15c7594ad891470c3cb
Hélio Borges <helio.borgesjr@gmail.com>
2023-01-06 13:27:34 -0300

    Render.com deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,17 +3,19 @@
 #bundle lock --add-platform x86_64-linux
 echo '************ Bundle Install ************'
 bundle install
-echo '************ Yarn ************'
-yarn
-echo '************ Yarn Adding Packages ************'
-yarn add @fortawesome/fontawesome-svg-core
-yarn add @fortawesome/free-solid-svg-icons
-yarn add @fortawesome/free-regular-svg-icons
-yarn add @fortawesome/free-brands-svg-icons
-echo '************ Yarn Build ************'
+#echo '************ Yarn ************'
+#yarn
+#echo '************ Yarn Adding Packages ************'
+#yarn add @fortawesome/fontawesome-svg-core
+#yarn add @fortawesome/free-solid-svg-icons
+#yarn add @fortawesome/free-regular-svg-icons
+#yarn add @fortawesome/free-brands-svg-icons
+#echo '************ Yarn Build ************'
 yarn build
+echo '************ Assets:Clobber ************'
+bundle exec rails rake assets:clobber
 echo '************ Assets:Precompile ************'
-bundle exec rails assets:precompile
+bundle exec rails assets:precompile --trace
 #yarn build
 #rails active_storage:install
 #bundle exec rake assets:precompile
