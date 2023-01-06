8da22a27ee920c0d2fa6b6ff86ac9906d8e2764a
Hélio Borges <helio.borgesjr@gmail.com>
2023-01-06 11:59:44 -0300

    Render.com deploy

--- db/migrate/20221214205318_add_file_cv_to_resume_info.rb
+++ db/migrate/20221214205318_add_file_cv_to_resume_info.rb
@@ -1,5 +1,5 @@
 class AddFileCvToResumeInfo < ActiveRecord::Migration[7.0]
   def change
-    add_column :resume_infos, :file_cv, :attachment
+    add_column :resume_infos, :file_cv, :binary
   end
 end
