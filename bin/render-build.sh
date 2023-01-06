b9d8130ca59b1e0125e85195eebc01192372e856
Hélio Borges <helio.borgesjr@gmail.com>
2023-01-06 13:08:08 -0300

    Render.com deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,13 @@
 #bundle config unset frozen
 #bundle lock --add-platform ruby
 #bundle lock --add-platform x86_64-linux
-bundle install
+#bundle install
+yarn
+yarn build
+bundle exec rails assets:precompile
 #yarn build
 #rails active_storage:install
 #bundle exec rake assets:precompile
 #bundle exec rake assets:clean
-bundle exec rake db:migrate
+#bundle exec rake db:migrate
 #bundle exec rake db:seed
