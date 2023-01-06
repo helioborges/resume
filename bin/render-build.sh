3410eae3980cc910e27b0d4740ebd280140a0ecb
Hélio Borges <helio.borgesjr@gmail.com>
2023-01-06 13:16:14 -0300

    Render.com deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 echo '************ Yarn ************'
 yarn
 echo '************ Yarn Adding Packages ************'
-yarn add @fortawesome/fontawesome-svg-
+yarn add @fortawesome/fontawesome-svg-core
 yarn add @fortawesome/free-solid-svg-icons
 yarn add @fortawesome/free-regular-svg-icons
 yarn add @fortawesome/free-brands-svg-icons
