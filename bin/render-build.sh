647789ccb88ede1496bdeb95063bcfef4d3964c5
Hélio Borges <helio.borgesjr@gmail.com>
2023-01-06 10:50:27 -0300

    Sending Gemfile.lock

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+bundle config unset frozen 
+bundle lock --add-platform ruby  
+bundle lock --add-platform x86_64-linux  
+bundle install 
+rails active_storage:install 
+bundle exec rake assets:precompile 
+bundle exec rake assets:clean 
+bundle exec rake db:migrate  
+bundle exec rake db:seed  
