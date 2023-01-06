e63a60087d43646a3cb74526aae7fec9d86a124b
Hélio Borges <helio.borgesjr@gmail.com>
2023-01-06 11:44:03 -0300

    Render.com deploy

--- db/migrate/20221129165548_create_resume_social_network.rb
+++ db/migrate/20221129165548_create_resume_social_network.rb
@@ -1,7 +1,7 @@
 class CreateResumeSocialNetwork < ActiveRecord::Migration[7.0]
   def change
     create_table :resume_social_networks do |t|
-      t.references :social_netork, null: false, foreign_key: true
+      #t.references :social_network, null: false, foreign_key: true
       t.string :url
       t.boolean :show
 
--- db/migrate/20221212131966_add_resume_info_id_to_resume_social_network.rb
+++ db/migrate/20221212131966_add_resume_info_id_to_resume_social_network.rb
@@ -1,7 +1,7 @@
 class AddResumeInfoIdToResumeSocialNetwork < ActiveRecord::Migration[7.0]
 
   def change
-    remove_column :resume_social_networks, :social_netork_id
+    # remove_column :resume_social_networks, :social_netork_id
     add_reference :resume_social_networks, :social_network, null: false, foreign_key: true
     add_reference :resume_social_networks, :resume_info, null: false, foreign_key: true
   end
